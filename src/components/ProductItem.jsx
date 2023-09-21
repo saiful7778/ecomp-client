9b712e74d003ac04a0d6c364c671e99f79248a8d
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-09-21 21:39:40 +0600

    style product showcase item

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -6,17 +6,19 @@ const ProductItem = ({ data }) => {
     console.log('product id is ', id);
   }
   return (
-    <div className='p-3 border border-gray-600 rounded-lg'>
-      <figure className='w-full h-52 bg-gray-50 p-3 rounded-lg'>
-        <img className='w-full h-full object-contain' src={image} alt={`${title} image`} />
-      </figure>
-      <h4>{title}</h4>
-      <div>{price}</div>
-      <div className="flex justify-between">
-        <div>Rating: {rating.rate}</div>
-        <div>Count: {rating.count}</div>
+    <div className='flex flex-col gap-4 p-3 bg-gray-300 dark:bg-gray-800 border border-gray-400 dark:border-gray-600 rounded-lg'>
+      <div className="flex-1 space-y-2">
+        <figure className='w-full h-40 bg-gray-50 border border-gray-400 dark:border-gray-600 p-6 rounded-lg'>
+          <img className='w-full h-full object-contain' src={image} alt={`${title} image`} />
+        </figure>
+        <h4 className='text-lg font-bold'>{title}</h4>
+        <div className=''>Price: <span className='text-sky-500 font-bold'>${price}</span></div>
+        <div className="flex justify-between text-sm text-gray-500">
+          <div>Rating: {rating.rate}</div>
+          <div>Count: {rating.count}</div>
+        </div>
       </div>
-      <button onClick={addToCartBtn} type="button">Add to cart</button>
+      <button onClick={addToCartBtn} className='p-3 font-bold bg-sky-400 dark:bg-sky-600 rounded-lg hover:underline' type="button">Add to cart</button>
     </div>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,8 +7,8 @@ const Products = () => {
   const renderAllProducts = data.map((ele) => <ProductItem key={uid()} data={ele} />)
   return (
     <div>
-      <h2 className="my-4 text-2xl font-medium">All products here:</h2>
-      <div className="grid grid-cols-4 gap-2">
+      <h2 className="my-4 text-4xl font-medium">All products here:</h2>
+      <div className="grid grid-cols-4 gap-4">
         {renderAllProducts}
       </div>
     </div>
