1f7dbab67b9a349f00ab7b0b370572d813d0b5df
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-17 18:59:04 +0600

    make btn comp

--- src/components/PhoneItem.jsx
+++ src/components/PhoneItem.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const PhoneItem = ({ itemData }) => {
@@ -10,31 +9,17 @@ const PhoneItem = ({ itemData }) => {
     type,
     brandName,
     price,
-    releaseData,
-    androidVersion,
-    osVersion,
-    bodyWeight,
-    bodyThickness,
-    batteryCapacity,
     processor,
-    ram,
-    storage,
-    memoryCard,
-    disRes,
-    disSize,
-    camRes,
     review,
-    productImages,
   } = itemData || {};
-  const navigate = useNavigate();
   const addToCartBtn = () => {
     console.log("product id is ", _id);
-    navigate(`/phones/${_id}`);
+    // navigate(`/phones/${_id}`);
   };
   return (
     <>
-      <div className="space-y-2 p-3 container-bg rounded-lg">
-        <figure className="w-full h-48 overflow-hidden bg-gray-50 border border-color rounded-lg">
+      <div className="p-3 container-bg rounded-lg shadow-lg">
+        <figure className="w-full h-48 mb-2 overflow-hidden bg-gray-50 border border-color rounded-lg">
           <img
             className="w-full h-full object-contain"
             src={thumbnail}
@@ -44,10 +29,10 @@ const PhoneItem = ({ itemData }) => {
         <Link to={`/phones/${_id}`}>
           <h5 className="text-2xl font-bold hover:underline">{phoneName}</h5>
         </Link>
-        <div className="text-xl capitalize font-semibold text-sky-600">
-          ${price}
+        <div className="capitalize font-semibold">
+          Price: <span className="text-sky-500 text-xl">${price}</span>
         </div>
-        <div className="grid grid-cols-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="grid grid-cols-2 my-2 text-sm text-gray-500 dark:text-gray-400">
           <div>
             Type: <span className="capitalize font-semibold">{type}</span>
           </div>
@@ -62,11 +47,17 @@ const PhoneItem = ({ itemData }) => {
             <span className="capitalize font-semibold">{processor}</span>
           </div>
         </div>
-        <div className="flex items-center justify-between">
-          <button type="button">View details</button>
+        <div className="flex items-center justify-between gap-2 mt-3">
+          <Link
+            to={`/phones/${_id}`}
+            className="btn btn-pri w-full"
+            type="button"
+          >
+            View details
+          </Link>
           <button
             onClick={addToCartBtn}
-            className="p-3 font-bold capitalize text-gray-50 bg-sky-600 rounded-lg hover:underline"
+            className="btn btn-pri-outline w-full"
             type="button"
           >
             add to cart
